32a7e91c1ec2fc9ad0b1b14435a45ae7943f4ed5
AchmadAlvin <achmadalvin886@gmail.com>
2026-05-17 21:39:09 +0700

    Create AuthViewModel

---
+++ app/src/main/java/com/example/kafecraft/ui/auth/AuthViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.kafecraft.ui.auth
+
+import android.media.session.MediaSessionManager
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel(private val sessionManager: MediaSessionManager) : ViewModel() {
+
+}
