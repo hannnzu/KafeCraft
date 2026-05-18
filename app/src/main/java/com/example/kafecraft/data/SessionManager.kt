aa4d9ba744d624fdb7b324f7dd3ce82650abbdfd
AchmadAlvin <achmadalvin886@gmail.com>
2026-05-18 23:48:10 +0700

    Update SessionManager to handle user session and logout

--- app/src/main/java/com/example/kafecraft/data/SessionManager.kt
+++ app/src/main/java/com/example/kafecraft/data/SessionManager.kt
@@ -4,13 +4,30 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class SessionManager(context: Context) {
-    private val SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+    private val prefs : SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
 
     companion object {
         private const val PREF_NAME = "kafecraft_session"
-        private const val KEY_IS_LOGGED_IN = "user_id"
-        private const val KEY_USER_ID = "user_email"
+        private const val KEY_USER_ID = "user_id"
+        private const val KEY_USER_NAME = "user_name"
+        private const val KEY_USER_EMAIL = "user_email"
     }
 
+
+    // 71. Fungsi public 'saveLoginSession' mengelola input parameter String.
+    fun saveLoginSession(userId: String, userName: String, userEmail: String) {
+        prefs.edit().apply {
+            putString(KEY_USER_ID, userId)
+            putString(KEY_USER_NAME, userName)
+            putString(KEY_USER_EMAIL, userEmail)
+            apply()
+        }
+    }
+
+    fun getUserId(): String? = prefs.getString(KEY_USER_ID, null)
+    fun getUserName(): String? = prefs.getString(KEY_USER_NAME, null)
+    fun getUserEmail(): String? = prefs.getString(KEY_USER_EMAIL, null)
+
+    fun logout() { prefs.edit().clear().apply() }
 
 }
